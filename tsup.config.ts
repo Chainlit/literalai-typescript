dd06c76c411096c4f7fe695a49688946ed7a5986
Willy Douhard <willy.douhard@gmail.com>
2024-03-22 10:21:57 +0100

    reduce bundle size

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   target: 'es2020',
   format: ['cjs', 'esm'],
-  splitting: false,
-  sourcemap: true,
+  splitting: true,
+  sourcemap: false,
   clean: true,
   dts: true
 });
